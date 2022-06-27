67ec71d9b6c992418b4723c67640d69f86f82e88
imcoco <imcoco@gmail.com>
2022-06-27 12:49:46 -0400

    implement review model

---
+++ models/review.js
@@ -0,0 +1,20 @@
+
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const reviewSchema = new Schema({
+  content: String,
+  rating: Number,
+  createdBy: {type: Schema.Types.ObjectId, ref: "Profile"},
+  restaurant: {type: Schema.Types.ObjectId, ref: "Restaurant"},
+}, {
+  timestamps: true
+}
+)
+
+const Review = mongoose.model('Review', reviewSchema)
+
+export {
+  Review
+}
