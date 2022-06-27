c40a586ebed04ad2528c3f6b5760046dc9657949
imcoco <imcoco@gmail.com>
2022-06-27 13:39:34 -0400

    implement index function

--- controllers/restaurants.js
+++ controllers/restaurants.js
@@ -1,7 +1,19 @@
 import { Restaurant } from '../models/restaurant.js'
 
 function index(req, res){
+  Restaurant.find({})
+  .then(restaurants => {
+    res.render('restaurants/index', {
+      restaurants,
+      title: "Restaurants",
+      user: req.user ? req.user: null
 
+    })
+  })
+  .catch(err => {
+    console.log(err)
+    res.redirect('/')
+  })
 }
 
 export {
