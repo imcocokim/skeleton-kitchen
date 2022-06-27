1ccc35695063ae87d182e8d83a8fd4e329473024
imcoco <imcoco@gmail.com>
2022-06-27 13:27:13 -0400

    define restaurant route on server

--- server.js
+++ server.js
@@ -17,6 +17,7 @@ import('./config/passport.js')
 // require routes
 import { router as indexRouter } from './routes/index.js'
 import { router as authRouter } from './routes/auth.js'
+import { router as restaurantRouter } from './routes/restaurants.js'
 
 // create the express app
 const app = express()
@@ -58,6 +59,7 @@ app.use(passport.session())
 // router middleware
 app.use('/', indexRouter)
 app.use('/auth', authRouter)
+app.use('/restaurants', restaurantRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
