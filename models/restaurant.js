9a9bc49d3cb179e04f77124ec9d77445ccbf2584
imcoco <imcoco@gmail.com>
2022-06-27 12:49:17 -0400

    implement restaurant model

---
+++ models/restaurant.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose"
+
+const Schema = mongoose.Schema
+
+const restaurantSchema = new Schema({
+  name: String,
+  location: String,
+  cuisine: String,
+  reviews: {type: Schema.Types.ObjectId, ref: "Reviews"},
+  createdBy: {type: Schema.Types.ObjectId, ref: "Profile"}
+}, {
+  timestamps: true
+})
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema)
+
+export {
+  Restaurant
+}
+
